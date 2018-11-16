5e93dbf5aa5db9121e1bf30e73cd54dfcd622885
Goldsipa <goldsip@gmail.com>
2018-11-16 19:27:02 +0200

    Updates

---
+++ precache-manifest.82882a7017c2a64e0b1f6ce2d7f326bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-mobile-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f9ebc9c7e84ec0291407",
+    "url": "/react-mobile-test/static/js/runtime~main.f9ebc9c7.js"
+  },
+  {
+    "revision": "ca3a43a1ca65f65d95c6",
+    "url": "/react-mobile-test/static/js/main.ca3a43a1.chunk.js"
+  },
+  {
+    "revision": "da465a6fbec3092777fc",
+    "url": "/react-mobile-test/static/js/1.da465a6f.chunk.js"
+  },
+  {
+    "revision": "ca3a43a1ca65f65d95c6",
+    "url": "/react-mobile-test/static/css/main.037602cd.chunk.css"
+  },
+  {
+    "revision": "4329d5a506603daf31e3cac8d68abca3",
+    "url": "/react-mobile-test/index.html"
+  }
+];
